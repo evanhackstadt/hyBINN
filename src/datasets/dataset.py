1947595ca2f65fcad49358597f57170ce2beebe1
Evan Hackstadt <evan.hackstadt@gmail.com>
2026-03-14 23:29:41 -0700

    write dataset.py

--- src/datasets/dataset.py
+++ src/datasets/dataset.py
@@ -1,4 +1,39 @@
-import pandas as pd
 import torch
+from torch.utils.data import Dataset
+import pandas as pd
+import numpy as np
 
-# class SurvivalDataset(torch.utils.data.Dataset):
+class SurvivalDataset(Dataset):
+    
+    def __init__(self, csv_path: str):
+        """
+        csv_path: path to the preprocessed data CSV file
+        """
+        
+        df = pd.read_csv(csv_path, index_col=0)
+        df_features = df.drop(columns=['OS.time', 'OS'])
+        df_labels = df[['OS.time', 'OS']]
+        print(df_features.head())
+        print(df_labels.head())
+        
+        # covert dataframe --> numpy
+        features = df_features.to_numpy(dtype=np.float32)
+        labels = df_labels.to_numpy(dtype=np.float32)
+        
+        # convert numpy --> tensors
+        self.x = torch.from_numpy(features)
+        self.y = torch.from_numpy(labels)    # time, event
+        self.n_samples = df.shape[0]
+        self.feature_dim = self.x.shape[1]
+        self.gene_cols = list(df_features.columns)
+
+    def __len__(self):
+        return self.n_samples
+
+    def __getitem__(self, idx):
+        item = {
+            'x': self.x[idx],
+            'time': self.y[idx, 0],
+            'event': self.y[idx, 1]
+        }
+        return item
