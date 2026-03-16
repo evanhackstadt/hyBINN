b7ed8c9706bc58adebc72a6ccfbc554d812771e1
Evan Hackstadt <evan.hackstadt@gmail.com>
2026-03-15 23:40:13 -0700

    write mapping matrix function

--- src/processing/reactome.py
+++ src/processing/reactome.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 # BUILD REACTOME MAP
@@ -11,42 +12,26 @@ def build_reactome_map(path):
         path (str): filepath to the Ensembl2Reactome text file from Reactome
     
     Returns:
-        map (dict): given the Ensembl ID of a gene, what Reactome pathways does it map to
+        pathway_map (dict): given the Ensembl ID of a gene, what Reactome pathways does it map to
     """
     
     df = pd.read_csv(path, delimiter='\t',
                      names=["EnsemblID", "ReactomePathwayID", "URL", 
                             "PathwayName", "Evidence", "Species"])
     
-    print("--RAW--")
-    print(df.shape)
-    print(df.head())
-    
     # Filter to only human pathways
     df2 = df[df["Species"] == "Homo sapiens"]
-    
-    print("--HUMAN--")
-    print(df2.head())
-    print(df2.shape)
     
     # Initial filter to pathways with 15 ≤ n_genes ≤ 300
     # Will perform this filter again based on dataset genes
     pathway_sizes = df2.groupby("ReactomePathwayID")["EnsemblID"].nunique()
     valid_pathways = pathway_sizes[(pathway_sizes >= 15) & (pathway_sizes <= 300)].index
     df3 = df2[df2["ReactomePathwayID"].isin(valid_pathways)]
-
-    print("--COUNTS--")
-    print(df3.shape)
-    print(df3.head())
     
     # Filter to only EnsemblIDs we are using
     indices_to_drop = [row.Index for row in df3.itertuples()
-                       if row.EnsemblID.startswith("ENSG")]
+                       if not row.EnsemblID.startswith("ENSG")]
     df4 = df3.drop(index=indices_to_drop)
-    
-    print("--IDs--")
-    print(df4.shape)
-    print(df4.head())
     
     # Aggregate pathways per gene
     pathway_map = df4.groupby("EnsemblID")["ReactomePathwayID"].apply(list).to_dict()
@@ -56,5 +41,37 @@ def build_reactome_map(path):
 
 # BUILD SPARSE MASK MATRIX
 # This will zero-out weights between genes and pathways they aren't part of
-def build_mask_matrix():
-    return
+def build_mask_matrix(mapped_genes, pathway_map, valid_pathways):
+    """
+    Builds the mask matrix that determines the sparse mapping of genes to pathway nodes
+    
+    Args:
+        mapped_genes (list): the filtered list of Ensembl IDs that map to Reactome pathways
+        pathway_map (dict): a dictionary mapping Ensemble IDs to lists of Reactome pathways
+        valid_pathways (list): the list of ReactomePathwayIDs from filter_pathway_map
+    
+    Returns:
+        mask_matrix (ndarray): a binary matrix of size [n_mapped_genes x n_pathways]; M[i,j] = 1 if gene i is part of pathway j
+        sorted_genes: mapped_genes, sorted - the row index of the matrix
+        sorted_pathways: valid_pathways, sorted - the columns of the matrix
+    """
+    
+    # row / column labels
+    sorted_genes = sorted(mapped_genes)
+    sorted_pathways = sorted(valid_pathways)
+    
+    # row / column index lookups
+    gene_idx = {val: idx for idx, val in enumerate(sorted_genes)}
+    pathway_idx = {val: idx for idx, val in enumerate(sorted_pathways)}
+    
+    M = np.zeros(shape=[len(mapped_genes), len(valid_pathways)], dtype=np.float32)
+    
+    for gene, pathways in pathway_map.items():
+        if gene not in gene_idx:
+            continue
+        for pathway in pathways:
+            if pathway not in pathway_idx:
+                continue
+            M[gene_idx[gene], pathway_idx[pathway]] = 1.0
+    
+    return M, sorted_genes, sorted_pathways
